c8e44f76b804574033546781b97727154f498fab
Nomera67 <y.rechtenstein@gmail.com>
2023-04-24 17:24:03 +0200

    add landing page and home page. Design tests and beginning of colors schemes choice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./features/components/landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: 'home', loadChildren: () => import('./features/components/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
