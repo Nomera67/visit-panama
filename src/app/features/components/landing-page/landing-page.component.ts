533d048d2e0f275f3dd0166d630f694689c2a5d7
Nomera67 <y.rechtenstein@gmail.com>
2023-04-26 16:52:53 +0200

    add transition effect, new design of CTA, add lorem on home page to test transition between landing and home

--- src/app/features/components/landing-page/landing-page.component.ts
+++ src/app/features/components/landing-page/landing-page.component.ts
@@ -13,8 +13,24 @@ export class LandingPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  toHome(){
-    this._router.navigate(['/home']);
+  
+
+  toHome() {
+    const button = document.getElementById('homeButton');
+    const svg = document.getElementById('homeSvg');
+    const container = document.getElementById('container');
+    const hiderContainer = document.getElementById('container--hide');
+
+
+    button?.classList.add('hideCTA');
+    svg?.classList.add('hideSvg');
+    container?.classList.add('hideContainer');
+    hiderContainer?.classList.add('disappearActive');
+    setTimeout(() => {
+      // Redirection vers la page visée
+      this._router.navigate(['/home']);
+    }, 7000);
   }
+  
 
 }
