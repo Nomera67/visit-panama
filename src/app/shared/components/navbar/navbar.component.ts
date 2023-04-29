73c2fe8aec85d1b9f1c6f82f7bf993ca788e91db
Nomera67 <y.rechtenstein@gmail.com>
2023-04-29 23:15:31 +0200

    add low resolution navbar and some details. Lot need to be done in design

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -13,6 +13,21 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleNav(){
+    const primaryNav = document.getElementById("primary-navigation");
+    const toggleButton = document.getElementById('toggleButton');
+    const logo = document.getElementById('logoItem');
+    if(primaryNav?.getAttribute("data-visible") === "false"){ //if navbar is not visible
+      primaryNav.setAttribute("data-visible", "true"); //let it appear with the attribute
+      toggleButton?.setAttribute("aria-expanded", "true"); //and tell it to screen readers
+      logo?.setAttribute("data-color", "true");
+    } else {
+      primaryNav?.setAttribute("data-visible", "false"); //else let it disappear with removing the attribute
+      toggleButton?.setAttribute("aria-expanded", "false") //and tell it to screen readers
+      logo?.setAttribute("data-color", "false");
+    }
+  }
+
   toHome(){
     this._router.navigate(['/home']);
   }
