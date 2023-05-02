f359f9c768d276b3912390debe043d240018a297
Nomera67 <y.rechtenstein@gmail.com>
2023-05-02 22:10:38 +0200

    change background heroe on homepage, font and add parallax effect on scroll

--- src/app/features/components/home/home.component.ts
+++ src/app/features/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+  }
+  titlePos: number = 0;
+  subTitlePos: number = 0;
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const homeTitle = this.elementRef.nativeElement.querySelector('.home__title > h1');
+    const homeSubtitle = this.elementRef.nativeElement.querySelector('.home__title > p');
+    const homeContainer = this.elementRef.nativeElement.querySelector('.home__main');
+    
+    // Calculer la nouvelle position verticale du titre et du sous-titre
+    this.titlePos = Math.round((homeContainer.getBoundingClientRect().top * -1) / 2);
+    this.subTitlePos = Math.round((homeContainer.getBoundingClientRect().top * -1) / 4);
+
+    // Appliquer les nouvelles positions verticales
+    homeTitle.style.transform = `translateY(${this.titlePos}px)`;
+    homeSubtitle.style.transform = `translateY(${this.subTitlePos}px)`;
   }
 
 }
