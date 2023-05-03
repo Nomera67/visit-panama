6b06be41deb856a4b3dd8a6d211dbdfcb22c66d5
Nomera67 <y.rechtenstein@gmail.com>
2023-05-03 21:26:19 +0200

    change display on homepage & add introduction component to it

--- src/app/features/components/home/home.module.ts
+++ src/app/features/components/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    IntroductionComponent
   ],
   imports: [
     CommonModule,
