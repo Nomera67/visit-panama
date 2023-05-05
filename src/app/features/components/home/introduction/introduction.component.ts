44d82fe74d1e50e75171afc62ae112a3114ee3fc
Nomera67 <y.rechtenstein@gmail.com>
2023-05-05 18:18:38 +0200

    add IntersectionObserver to detect viewport and let appear differents parts of homepage with animation

--- src/app/features/components/home/introduction/introduction.component.ts
+++ src/app/features/components/home/introduction/introduction.component.ts
@@ -10,6 +10,26 @@ export class IntroductionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const geography = document.querySelector('#geography');
+    const heritage = document.querySelector('#heritage');
+    const biodiversity = document.querySelector('#biodiversity');
+    if (geography && heritage && biodiversity) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            setTimeout(() => {
+              entry.target.classList.add('is-visible');
+            }, 200);
+          } else {
+            setTimeout(() => {
+              entry.target.classList.remove('is-visible');
+            }, 200);
+          }
+        });
+      });
+      observer.observe(geography);
+      observer.observe(heritage);
+      observer.observe(biodiversity);
+    }
   }
-
 }
