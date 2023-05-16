eb996c12d75b989e26affd6aacdfae6f9be49e50
Nomera67 <y.rechtenstein@gmail.com>
2023-05-16 16:41:57 +0200

    add appear with observer

--- src/app/features/components/tourisme/invitation/invitation.component.ts
+++ src/app/features/components/tourisme/invitation/invitation.component.ts
@@ -10,6 +10,33 @@ export class InvitationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const hook = document.querySelector('#invitationHook');
+    const culture = document.querySelector('#invitationCulture');
+    const invitationBiodiversity = document.querySelector('#invitationBiodiversity');
+    const beach = document.querySelector('#invitationBeach');
+    const activity = document.querySelector('#invitationActivity');
+    const canal = document.querySelector('#invitationCanal');
+    if (hook && culture && invitationBiodiversity && beach && activity && canal){
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting){
+            setTimeout(() => {
+              entry.target.classList.add('is-visible');
+            }, 200);
+          } else {
+            setTimeout(() => {
+              entry.target.classList.remove('is-visible');
+            }, 200);
+          }
+        });
+      });
+      observer.observe(hook);
+      observer.observe(culture);
+      observer.observe(invitationBiodiversity);
+      observer.observe(beach);
+      observer.observe(activity);
+      observer.observe(canal);
+    }
   }
 
 }
